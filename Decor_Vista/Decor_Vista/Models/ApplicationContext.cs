551d7ee4a13aaeab6f51032109747702728b2ce7
M. Sami <154611254+Code-With-Sami@users.noreply.github.com>
2025-08-11 13:44:45 +0500

    installation

---
+++ Decor_Vista/Decor_Vista/Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Decor_Vista.Models
+{
+    public class ApplicationContext : DbContext 
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+             
+        }
+    }
+}
